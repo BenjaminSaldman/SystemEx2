5fc54a9aaee01a223659c1df75b9ea058c228571
BenjaminSaldman <93070344+BenjaminSaldman@users.noreply.github.com>
2021-11-22 17:51:13 +0200

    Version 5

--- main.c
+++ main.c
@@ -4,19 +4,21 @@ int main(){
     char c='A';
     int mat[10][10];
     int i=0,j=0;
-    scanf("%c",&c);
+    scanf(" %c",&c);
     while(c!='D')
     {
         switch (c){
             case 'A': inputToMat(mat,10);
                         break;
-            case 'B': path(mat,10,i,j);
+            case 'B':   scanf("%d %d",&i,&j);
+                        path(mat,10,i,j);
                         break;
-            case 'C': shortestPath(mat,10,i,j);
+            case 'C':   scanf("%d %d",&i,&j);
+                        shortestPath(mat,10,i,j);
                         break;
 
         }
-        scanf("%c",&c);
+        scanf(" %c",&c);
     }
     printf("\n");
     return 0;
